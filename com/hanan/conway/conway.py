87a77ef9e10de35f1873e5f8be6c9dcc5de3fc12
Hanan Wiener <Hanan Wiener>
2017-01-15 12:06:33 +0200

    mvc

--- com/hanan/conway/conway.py
+++ com/hanan/conway/conway.py
@@ -1,14 +1,19 @@
 import collections
 import logging
+import os
+from tempfile import gettempdir
+
 
 log = logging.getLogger('conway')
-file_handler = logging.FileHandler('conway.log', mode='w')
+file_handler = logging.FileHandler(os.path.join(gettempdir(), 'conway.log'), mode='w')
 log.addHandler(file_handler)
 log.setLevel(logging.DEBUG)
 
 
 class IConwayView(object):
     def is_alive(self, x, y):
+        raise NotImplementedError('THIS IS AN INTERFACE')
+    def get_gen_num(self):
         raise NotImplementedError('THIS IS AN INTERFACE')
     
 class IConwayGame(object):
@@ -22,7 +27,7 @@ class Cell:
     
 class Conway(IConwayGame, IConwayView):
     def __init__(self, live_cells):
-        log.debug('Conway __init__')
+        self.gen_num = 0
         self.current_live_cells = dict()
         for c in live_cells:
             self.current_live_cells[(c.x,c.y)] = True
@@ -35,7 +40,6 @@ class Conway(IConwayGame, IConwayView):
                 if not included and i == x and j == y:
                     continue
                 yield (i,j)
-                        
                     
     def is_alive(self,x,y):
         alive = False
@@ -44,38 +48,28 @@ class Conway(IConwayGame, IConwayView):
         except KeyError:
             pass
         return alive
+    
     def play(self):
-        log.debug('play starts')
-#         log.debug('oscil: {}'.format(self.osc))
         next_gen = dict()
         process_set = set()
         for (x,y) in self.current_live_cells.keys():
-            log.debug('checking out live cell ({},{})'.format(x,y))
             nbr_set = set([nbr for nbr in Conway.neighbours(x,y, included=True)])
-            log.debug('neighbours set:')
-            for _nbr in nbr_set:
-                log.debug(_nbr)
             process_set = process_set.union(nbr_set)
-            log.debug('united set : {}'.format(nbr_set))
-#             print('process_set')
-#             print(process_set)
-        log.debug('process set : {}'.format(process_set))                        
         for (x,y) in process_set:
-            log.debug('checking cell in ({},{})'.format(x,y))
-#             x,y =_pair.x, _pair.y
             num_live_nbrs = 0
             for (nbrx, nbry) in Conway.neighbours(x,y):
                 if self.is_alive(nbrx,nbry):
                     num_live_nbrs += 1
-                    log.debug('neighbour ({},{}) is alive'.format(nbrx,nbry))
             processed_cell_alive= self.is_alive(x,y)
-            log.debug('determine state for {} cell ({},{}) with {} lively lovely neighbours'.format(
-                'Live' if processed_cell_alive else 'Dead', x,y, num_live_nbrs))
             next_gen_alive = Conway.apply_rules(processed_cell_alive, num_live_nbrs)
-            log.debug('({},{}) will be {} in the next generation'.format(x,y, 'Live' if next_gen_alive else 'Dead'))
             if next_gen_alive:
                 next_gen[(x,y)] = next_gen_alive
         self.current_live_cells = next_gen
+        self.gen_num += 1
+
+    def get_gen_num(self):
+        return self.gen_num
+    
     @staticmethod
     def apply_rules(alive, num_live_nbrs):
         if alive:
@@ -91,17 +85,12 @@ class Conway(IConwayGame, IConwayView):
             else:
                 return False
             
-            
-            
-            
-            
         
 class ConwayUi(object):
-    def __init__(self, conway_game):
-        self.conway_game = conway_game
-        self.oscil = 0
-    def play(self):
+    def __init__(self):
         pass
+    def set_game(self, conway_game):
+        self.conway_game = conway_game
     def show(self):
         pass
     
@@ -113,9 +102,11 @@ class ConsoleUi(ConwayUi):
     HIGHER_VER = DIAMETER
     LIVE_CELL = '*'
     DEAD_CELL = ' '
-    HEADER_CHAR = 'C'   
-    def __init__(self, conway_game):
-        ConwayUi.__init__(self, conway_game)
+    HEADER_CHAR = 'C'
+       
+    def __init__(self):
+        ConwayUi.__init__(self)
+        
     def show(self):
         while True:
             self.print_header()
@@ -123,7 +114,6 @@ class ConsoleUi(ConwayUi):
             try:
                 x = input('Press any key for next round, Ctrl+C to quit')
                 self.conway_game.play()
-                self.oscil += 1                    
             except KeyboardInterrupt:
                 raise 
             
@@ -133,27 +123,33 @@ class ConsoleUi(ConwayUi):
             for i in range(self.LOWER_HOR, self.HIGHER_HOR):
                 cl += self.LIVE_CELL if self.conway_game.is_alive(i,j) else self.DEAD_CELL
             print(cl)
+            
     def print_header(self):
         print('\n')
         print(self.HEADER_CHAR*(self.HIGHER_VER - self.LOWER_VER))
+        print('\n')
         print('Conway\'s Game of Life')
-        print('Oscillation {:0>10}'.format(self.oscil))
+        print('\n')
+        print('Generation {:0>10}'.format(self.conway_game.get_gen_num()))
+        print('\n')
         print(self.HEADER_CHAR*(self.HIGHER_VER - self.LOWER_VER))
         print('\n')
-                    
-                
-            
-            
-            
         
+class ConwayController(object):
+    def __init__(self, conway_game, conway_ui):
+        self.conway_ui = conway_ui
+        self.conway_ui.set_game(conway_game)
+    def start(self):
+        self.conway_ui.show()
+                    
 if __name__ == "__main__":
     
-    glider = 
-    '''
-     *
-      *
-    ***
-    '''
+#     glider = 
+#     '''
+#      *
+#       *
+#     ***
+#     '''
     
     
     
@@ -164,9 +160,11 @@ if __name__ == "__main__":
     live_cells.append(Cell(2,0))
     live_cells.append(Cell(2,-1))
     live_cells.append(Cell(1,-2))
-    conway = Conway(glider)
-    conwayUi = ConsoleUi(conway)
-    conwayUi.show()
+    conway_ui = ConsoleUi()
+    conway_game = Conway(live_cells)
+    ctrlr = ConwayController(conway_game, conway_ui)
+    ctrlr.start()
+    
         
     
     
